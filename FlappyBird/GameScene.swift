85e6580316b3e866532c2e69e9c837092684fae7
takatoshi.ichige <takatoshi.ichige>
2021-03-31 14:22:31 +0900

    地面の画像 (Texture)を無限スクロールさせる

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -9,21 +9,52 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    // スクロール用のノードをまとめたノード
+    var scrollNode: SKNode!
+    
     // 初期表示
     override func didMove(to view: SKView) {
         backgroundColor = UIColor(red: 0.15, green: 0.75, blue: 0.90, alpha: 1)
         
-        // 地面を表示
-        // Textureの生成
+        // スクロール用のノードをインスタンス化し、親Viewに設定
+        scrollNode = SKNode()
+        addChild(scrollNode)
+        
+        
+        // 地面Textureの生成
         let groundTexture = SKTexture(imageNamed: "ground")
         groundTexture.filteringMode = .nearest
         
-        // SpriteNodeを生成
-        let groundNode = SKSpriteNode(texture: groundTexture)
-        // ポジション設定
-        groundNode.position = .init(x: groundTexture.size().width / 2, y: groundTexture.size().height / 2)
         
-        // シーンに追加
-        addChild(groundNode)
+        // スクロール用にどのくらい画像を用意する必要があるか +2は右側で見切れないように追加している
+        let needScroolImageCount = Int(self.frame.size.width / groundTexture.size().width) + 2
+        
+        
+        // スクロールのアクションを作成
+        // ５秒かけて画像サイズ分左にずらす
+        let moveGround = SKAction.moveBy(x: -groundTexture.size().width, y: 0, duration: 5)
+        
+        // ０秒で元にもどすメソッド
+        let resetGround = SKAction.moveBy(x: groundTexture.size().width, y: 0, duration: 0)
+        
+        // リピートさせる
+        let repeatScrollGround = SKAction.repeatForever(SKAction.sequence([moveGround,resetGround]))
+        
+        
+        
+        // SpriteNodeを生成
+        //　必要な枚数分作成し、アクションを設定する
+        for i in 0..<needScroolImageCount {
+            let groundNode = SKSpriteNode(texture: groundTexture)
+            // ポジション設定
+            groundNode.position = .init(x: groundTexture.size().width / 2 + groundTexture.size().width * CGFloat(i),
+                                        y: groundTexture.size().height / 2)
+            
+            // アクションを設定
+            groundNode.run(repeatScrollGround)
+            
+            // シーンに追加
+            scrollNode.addChild(groundNode)
+        }
     }
 }
