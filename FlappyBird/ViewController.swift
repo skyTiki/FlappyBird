8e09a6a4da4b4ce59f1d25bac4b0a80d116e88c8
takatoshi.ichige <takatoshi.ichige>
2021-03-31 13:16:06 +0900

    skviewの表示（FPSとノード数を画面に出す）

--- FlappyBird/ViewController.swift
+++ FlappyBird/ViewController.swift
@@ -6,12 +6,23 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // ViewをSKviewに変換
+        let skView = self.view as! SKView
+        
+        // FPS,node数の表示
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
+        let scene = SKScene(size: skView.frame.size)
+        
+        skView.presentScene(scene)
     }
 
 
