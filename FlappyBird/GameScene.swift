e70beede5a9f60cf36c737a78494c7a51a50b1b3
takatoshi.ichige <takatoshi.ichige>
2021-03-31 13:42:45 +0900

    画像を表示させる（地面の画像）

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -12,5 +12,18 @@ class GameScene: SKScene {
     // 初期表示
     override func didMove(to view: SKView) {
         backgroundColor = UIColor(red: 0.15, green: 0.75, blue: 0.90, alpha: 1)
+        
+        // 地面を表示
+        // Textureの生成
+        let groundTexture = SKTexture(imageNamed: "ground")
+        groundTexture.filteringMode = .nearest
+        
+        // SpriteNodeを生成
+        let groundNode = SKSpriteNode(texture: groundTexture)
+        // ポジション設定
+        groundNode.position = .init(x: groundTexture.size().width / 2, y: groundTexture.size().height / 2)
+        
+        // シーンに追加
+        addChild(groundNode)
     }
 }
